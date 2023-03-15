9f239905bf18537ede9214418d4fe9c2f69bc333
Enoch <mmejuenoch@gmail.com>
2023-03-15 12:14:46 +0100

    I designed the UI for the header and counter options select tag.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  hello enoch
+  <header class="header">
+    <h1>
+      <p>THE</p>
+      <p>COUNTER APP</p>
+    </h1>
+    <p class="email">mmejuenoch@gmail.com</p>
+  </header>
+  <main class="main">
+    <form class="form-options" @submit.prevent="handleSelect">
+      <select class="select" @change="handleSelect">
+        <option value="manual">Manual Count</option>
+        <option value="range">Range Count</option>
+        <option value="word">Word Count</option>
+        <option value="info">Count Info</option>
+      </select>
+    </form>
+  </main>
 </template>
 
 <script>
   export default {
-    name: "App"
+    name: "App",
+    data(){
+      return {
+        selected: "manual",
+      }
+    },
+    methods: {
+      handleSelect(event) {
+        this.selected = event.target.value;
+      }
+    }
   }
 </script>
